792555983befb03100a739518a5fc39a14d9ef29
Mr. Meo <20571336+trananhmanh89@users.noreply.github.com>
2020-02-23 10:11:23 +0700

    update copyright

--- mod_ff_itune_feed.php
+++ mod_ff_itune_feed.php
@@ -3,7 +3,7 @@
  * @package     Itune Feed Display
  * @subpackage  mod_ff_itune_feed
  *
- * @copyright   https://funfis.com
+ * @copyright   https://github.com/trananhmanh89/mod-ff-music-chart
  * @license     GNU General Public License version 2 or later;
  */
 
